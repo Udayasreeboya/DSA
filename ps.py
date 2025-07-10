e2ab24b21a50e6301fb2ecc0884e900f4553b273
Boya Udayasree <145020333+Udayasreeboya@users.noreply.github.com>
2025-07-10 16:58:27 +0530

    Add files via upload

--- ps.py
+++ ps.py
@@ -1,64 +1,26 @@
-# a="dfa12312afg"
-# l=[]
-# for i in a:
-#     if i.isdigit():
-#         l.append(int(i))
-# # l=list(sorted(set(l)))
-# # print("max2:", l[-2])
 
+#rotate array
+def reverse(lst, i, j):
+    while i < j:
+        lst[i], lst[j] = lst[j], lst[i]
+        i += 1
+        j -= 1
+lst = [1, 2, 3, 4, 5, 6, 7]
+n = len(lst)
+k = 3 %n 
+reverse(lst, 0, n - 1)
+reverse(lst, 0, k - 1)
+reverse(lst, k, n - 1)
+print(lst)
 
-
-
-# def sec_max(l):
-#     max=-1
-#     max2=-1
-#     for i in l:
-#         if int(i) > max and int(i) > max2:
-#             max2=max
-#             max=int(i)
-#         if max>int(i)>max2:
-#             max2=int(i)
-#     return max2
-# print(sec_max(l))
-
-
-# l=[1,1,2]
-# res=[]
-# for i in l:
-#     if i not in res:
-#         res.append(i)
-# print(len(res))
-
-
-
-#revmove duplicated in a sorted list
-def removeDuplicates(nums):
-    i=0
-    for j in range(1,len(nums)):
-        if nums[i]!=nums[j]:
-            nums[i+1]=nums[j]
-            i+=1
-    return i+1
-print(removeDuplicates([1,1,2,2,4,6,6]))
-
-
-
-#find second max number if it is a digit
-# s="abc12324fjh"
-def secondmax(s):
-    max=-1
-    max2=-1
-    for i in s:
-        if i.isdigit():
-            if max < int(i):
-                max2=max
-                max=int(i)
-            elif max > int(i) > max2:
-                max2=int(i)
-    return max2
-print(secondmax("abc12324fjh"))
-
-
-
-
-    
+# check if array is sorted and rotated
+arr=[3,4,5,1,2,3,7]
+count=0
+n=len(arr)
+for i in range(0,n):
+    if arr[i]>arr[(i+1)%n]:
+        count+=1
+if count==1:
+    print(True)
+else:
+    print(False)
